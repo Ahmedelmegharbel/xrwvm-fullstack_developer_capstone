b8eb7d01a35c13f7e9f449cd0950bbea4da247fd
ahmedelmegharbel <ahmedelmegharbel@gmail.com>
2026-04-27 05:24:26 -0400

    fourth commit

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -6,10 +6,11 @@ from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
-    # # path for registration
+    # path for registration
 
     path(route='login', view=views.login_user, name='login'),
     path(route='logout', view=views.logout_request, name='logout'),
+    path(route='register', view=views.registration, name='register'),
 
     # path for dealer reviews view
 
