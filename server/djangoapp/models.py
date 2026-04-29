95115fa32b87c293b3907f210ac98ccb230c1f08
ahmedelmegharbel <ahmedelmegharbel@gmail.com>
2026-04-29 15:25:57 -0400

    12th commit

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -14,6 +14,7 @@ class CarMake(models.Model):
     def __str__(self):
         return self.name
 
+
 class CarModel(models.Model):
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
@@ -25,8 +26,8 @@ class CarModel(models.Model):
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
     year = models.IntegerField(default=2026,
-        validators=[
-            MaxValueValidator(2026), MinValueValidator(2015)
+    validators=[
+        MaxValueValidator(2026), MinValueValidator(2015)
         ]
     )
 
