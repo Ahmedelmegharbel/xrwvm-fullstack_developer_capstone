c5bc12d78e0cf76dee9f09e957fc7dd06b594277
ahmedelmegharbel <ahmedelmegharbel@gmail.com>
2026-04-29 09:43:51 -0400

    9th commit

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -11,9 +11,10 @@ sentiment_analyzer_url = os.getenv(
     'sentiment_analyzer_url',
     default="http://localhost:5050/")
 
+
 def get_request(endpoint, **kwargs):
     params = ""
-    if(kwargs):
+    if (kwargs):
         for key, value in kwargs.items():
             params = params+key+"="+value+"&"
 
@@ -24,7 +25,7 @@ def get_request(endpoint, **kwargs):
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         return response.json()
-    except:
+    except Exception as e:
         # If any error occurs
         print("Network exception occured")
 
@@ -46,6 +47,5 @@ def post_review(data_dict):
         response = requests.post(request_url, json=data_dict)
         print(response.json())
         return response.json()
-    except:
+    except Exception as e:
         print("Network exception occurred")
-
